59c3f0190118938f4c42a233817307b9c69e3b86
Tony N <4016013+T101QUT@users.noreply.github.com>
2018-06-01 08:51:23 +1000

    some udpates

--- keycloak/deploy.sh
+++ keycloak/deploy.sh
@@ -4,4 +4,4 @@ set -e
 DIR=$(dirname ${BASH_SOURCE[0]})
 source ${DIR}/.variables
 
-docker stack deploy --compose-file docker-compose.yml ${SERVICE_NAME}
+docker stack deploy --compose-file ${DIR}/docker-compose.yml ${SERVICE_NAME}
