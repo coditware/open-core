24b0d3cba84bbecc53f0446c3b48d9194bb206bf
Tony N <4016013+T101QUT@users.noreply.github.com>
2018-08-08 12:08:46 +1000

    removed traefik for re-evaluation

--- traefik/createNetwork.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-DIR=$(dirname ${BASH_SOURCE[0]})
-source ${DIR}/.variables
-
-docker network create --driver overlay --attachable ${NETWORK_NAME:-proxy}
--- traefik/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-DIR=$(dirname ${BASH_SOURCE[0]})
-source ${DIR}/.variables
-
-docker stack deploy --compose-file docker-compose.yml ${SERVICE_NAME:-traefik-proxy}
