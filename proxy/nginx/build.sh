4b4a2e1daff3c9f49d525cdb4f8ac4940a8c3411
Tony N <4016013+T101QUT@users.noreply.github.com>
2018-08-08 12:07:07 +1000

    custom image for nginx

---
+++ proxy/nginx/build.sh
@@ -0,0 +1,1 @@
+docker build -t coditware:nginx-alpine ./
---
+++ proxy/nginx/redeploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# set -e
+DIR=$(dirname ${BASH_SOURCE[0]})
+source ${DIR}/.variables
+
+declare SERVICE_NAME=${SERVICE_NAME:-"nginx-proxy"}
+declare IMAGE_NAME=${IMAGE_NAME:-"coditware:nginx-alpine"}
+declare VOLUME_NAME=${VOLUME_NAME:-"nginx-data"}
+
+docker image rm ${IMAGE_NAME}
+docker build -t ${IMAGE_NAME} ./
+
+docker stack rm ${SERVICE_NAME}
+
+docker volume rm ${VOLUME_NAME}
+docker volume create ${VOLUME_NAME}
+
+docker stack deploy --compose-file ${DIR}/docker-compose.yml ${SERVICE_NAME}
